15e6ac5cb6c92c84c4499121f3d3e78d46e8a7cc
Andrew Zaika <lytiy7404@gmail.com>
2025-01-02 21:17:09 +0200

    first commit

---
+++ src/main/java/shpp/azaika/App.java
@@ -0,0 +1,13 @@
+package shpp.azaika;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
