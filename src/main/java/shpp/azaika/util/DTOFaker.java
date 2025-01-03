464eabf1c4e4dcceebc2c4e031cd0f725df8ed87
Andrew Zaika <lytiy7404@gmail.com>
2025-01-03 17:58:40 +0200

    added first impl for DTO faker, now generates StoreDTO

---
+++ src/main/java/shpp/azaika/util/DTOFaker.java
@@ -0,0 +1,21 @@
+package shpp.azaika.util;
+
+import net.datafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import shpp.azaika.dto.StoreDTO;
+
+import java.util.Locale;
+
+public class DTOFaker {
+    private static final Logger log = LoggerFactory.getLogger(DTOFaker.class);
+    private final Faker faker;
+
+    public DTOFaker() {
+        this.faker = new Faker(Locale.of("uk_UA"));
+    }
+
+    public StoreDTO generateStore() {
+        return new StoreDTO(faker.address().fullAddress());
+    }
+}
