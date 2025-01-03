bc3cb8bfdb83b02b9eeb21f6d2dc698fd67c8aa0
Andrew Zaika <lytiy7404@gmail.com>
2025-01-03 17:36:56 +0200

    created StoreDTO

---
+++ src/main/java/shpp/azaika/dto/StoreDTO.java
@@ -0,0 +1,31 @@
+package shpp.azaika.dto;
+
+public class StoreDTO {
+    private long id;
+    private String address;
+
+    public StoreDTO(long id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+
+    public StoreDTO(String address) {
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
